80312761464eb5dd697f05656155a96bc0ae2c0d
Jack Stettner <jack@stet.com>
2017-10-18 18:47:17 -0400

    huge UI improvements, no longer built terribly

--- imports/ui/App.jsx
+++ imports/ui/App.jsx
@@ -1,17 +1,20 @@
 import React, { Component } from 'react';
 import Header from './Header';
-import SubmitPage from './Submit';
 import Post from './Post';
 import { createContainer } from 'meteor/react-meteor-data';
 import '../api/model.js';
 import { FilesCollection } from 'meteor/ostrio:files';
+
+const inputStyle = {
+  maxWidth: '300px',
+};
 
 class App extends Component {
   constructor() {
     super();
     this.state = {
       photo: null,
-      caption: "..."
+      caption: ""
     };
 
     this.submitEntry = this.submitEntry.bind(this);
@@ -38,29 +41,34 @@ class App extends Component {
       Posts.insert({
         photo: this.state.photo,
         timestamp: new Date(),
-        caption: (this.state.caption != "" ? this.state.caption : "...")
-      }, () => this.setState({photo: null, caption: "..."}));
+        caption: (this.state.caption != "" ? this.state.caption : "uncaptioned")
+      }, () => this.setState({photo: null, caption: ""}));
     }
   }
 
   render() {
     return(
-      <div className="container">
-        <div className="submit">
-          <input id="fileInput" type="file" accept="image/*" onChange={this.uploadPhoto.bind(this)}/>
-          <button onClick={this.submitEntry}>
-            Submit this entry
-          </button>
-          <div>
-          {this.state.photo &&
-            <img src={this.state.photo}/>
-          }
+      <div className="container column">
+        <Header />
+        <hr />
+        <form className="submit">
+          <h3>Submit a mess</h3>
+          <div className="form-group">
+            <label htmlFor="exampleInputFile">File input</label>
+            <input className="pb-5" id="fileInput" type="file" accept="image/*" onChange={this.uploadPhoto.bind(this)}/>
+            <p className="help-block">You must have a photo to submit.</p>
           </div>
-          <div>
-            <p>Caption: </p>
-            <input id="caption" type="text" value={this.state.caption} onChange={this.setCaption.bind(this)}/>
+          {this.state.photo && (
+            <div className="row">
+              <img src={this.state.photo}/>
+            </div>
+          )}
+          <div className="form-group">
+            <label htmlFor="exampleInputEmail1">Photo Caption</label>
+            <input type="text" className="form-control" placeholder="Enter a caption" style={inputStyle} value={this.state.caption} onChange={this.setCaption.bind(this)} />
           </div>
-        </div>
+          <button type="submit" onClick={this.submitEntry} className="btn btn-default">Submit</button>
+        </form>
         {this.props.posts.map((post) => (<Post key={post._id} src={post.photo} caption={post.caption} timestamp={post.timestamp}/>))}
       </div>
     );
--- imports/ui/Header.jsx
+++ imports/ui/Header.jsx
@@ -1,9 +1,14 @@
 import React from 'react';
 
+var rStyle = {
+  fontSize: '20px',
+  position: 'absolute'
+}
+
 const Header = (props) => (
-  <p props>
-    this is the header
-  </p>
+  <div className="header row">
+    <h1 className="col-md-3">MonitorMe<span style={rStyle}>®</span></h1>
+  </div>
 );
 
 export default Header;
